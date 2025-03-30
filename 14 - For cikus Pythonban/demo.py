230701d6db2de072fdf81d228e1d3c1515417263
vlevi5 <lev.vadasz@gmail.com>
2025-03-30 11:34:38 +0200

    4-es feladat és demo file létrehozása

---
+++ 14 - For cikus Pythonban/demo.py
@@ -0,0 +1,12 @@
+lista = ["alma", "citrom", "banán", "dinnye"]
+i = 0
+
+for elem in lista:
+    print(f"Index: {i} - Érték: {elem}")
+    i += 1
+
+# Kimenet:
+# Index: 0 - Érték: a
+# Index: 1 - Érték: c
+# Index: 2 - Érték: b
+# Index: 3 - Érték: d
